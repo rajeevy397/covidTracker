f4d66b1e4a1e1ae14671dd4d296707e45434ced0
Rajeev Yadav <rajeevy396@gmail.com>
2023-01-12 16:32:14 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <h1>Rajeev</h1>
+      <ContactManager data={contacts} />
+    </>
+  );
+};
+
+function AddPersonForm(props) {
+  const [person, setPerson] = useState('');
+
+  function handleChange(e) {
+    setPerson(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    props.handleSubmit(person);
+    setPerson('');
+    e.preventDefault();
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Add new contact"
+        onChange={handleChange}
+        value={person}
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
+}
+
+function PeopleList(props) {
+  const arr = props.data;
+  const listItems = arr.map((val, index) => <li key={index}>{val}</li>);
+  return <ul>{listItems}</ul>;
+}
+
+function ContactManager(props) {
+  const [contacts, setContacts] = useState(props.data);
+
+  function addPerson(name) {
+    setContacts([...contacts, name]);
+  }
+
+  return (
+    <div>
+      <AddPersonForm handleSubmit={addPerson} />
+      <PeopleList data={contacts} />
     </div>
   );
 }
+const contacts = ['James Smith', 'Thomas Anderson', 'Bruce Wayne'];
 
 export default App;
